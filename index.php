67f5e072485bdf772e8d5ec6a7b483a4e4a4d70e
Muzammil <muzzammilmuneer@gmail.com>
2017-03-21 19:15:25 +0500

    no message

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 	<?php
 	    $myPDO = pg_connect('host=ec2-54-235-90-107.compute-1.amazonaws.com dbname=dkf3l4nu5m7vj user=qbxcasondnmcds password=0b12931d71c65eee9a2fa303bcd8c1b460ff1878e7d0cc9f0c10a0ad563a85bc');
 	    $result = $myPDO->pg_query("SELECT * FROM Users");
-
+	    echo 'muzammil';
 
 		if(mysqli_num_rows($result) > 0 )
 		{
