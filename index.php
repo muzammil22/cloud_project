394ea730b055365d02ddb25ed6c8dd65e8f2a896
Muzammil <muzzammilmuneer@gmail.com>
2017-03-22 16:24:31 +0500

    no message

--- index.php
+++ index.php
@@ -1,27 +1,25 @@
 
     <?php
 	
-  //    $dbopts = parse_url(getenv('DATABASE_URL'));
-	 // $app->register(new Herrera\Pdo\PdoServiceProvider(),
-	 //                array(
-	 //                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-	 //                    'pdo.username' => $dbopts["user"],
-	 //                    'pdo.password' => $dbopts["pass"]
-	 //                )
-	 // );
+     $dbopts = parse_url(getenv('DATABASE_URL'));
+	 $app->register(new Herrera\Pdo\PdoServiceProvider(),
+	                array(
+	                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+	                    'pdo.username' => $dbopts["user"],
+	                    'pdo.password' => $dbopts["pass"]
+	                )
+	 );
 	echo 'muzammil';
 
-	    $myPDO = pg_connect('host=ec2-54-235-90-107.compute-1.amazonaws.com dbname=dkf3l4nu5m7vj user=qbxcasondnmcds password=0b12931d71c65eee9a2fa303bcd8c1b460ff1878e7d0cc9f0c10a0ad563a85bc');
-	    $result = $myPDO->pg_query("SELECT * FROM Users");
-	    $result = $result->execute();
-	    echo 'muzammil';
+		$st = $app['pdo']->prepare('SELECT name FROM test_table');
+	  $st->execute();
 
-		if(mysqli_num_rows($result) > 0 )
-		{
-			while($row=mysqli_fetch_array($result))
-			{
-				echo $row[0];
-			}
-		}
+	  $names = array();
+	  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+	    $app['monolog']->addDebug('Row ' . $row['name']);
+	    $names[] = $row;
+	  }
+	    echo 'muzammil2';
 
+		
 	?>
