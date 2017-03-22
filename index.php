809680735f3abda5f5e63f5647f28cb01455eac3
Muzammil <muzzammilmuneer@gmail.com>
2017-03-22 16:17:26 +0500

    no message

--- index.php
+++ index.php
@@ -9,16 +9,17 @@
 
     <h1>Hello world</h1>   
     
-<!--  //    $dbopts = parse_url(getenv('DATABASE_URL'));
-	// $app->register(new Herrera\Pdo\PdoServiceProvider(),
-	//                array(
-	//                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-	//                    'pdo.username' => $dbopts["user"],
-	//                    'pdo.password' => $dbopts["pass"]
-	//                )
-	// ); -->
+    <?php
+	
+  //    $dbopts = parse_url(getenv('DATABASE_URL'));
+	 // $app->register(new Herrera\Pdo\PdoServiceProvider(),
+	 //                array(
+	 //                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+	 //                    'pdo.username' => $dbopts["user"],
+	 //                    'pdo.password' => $dbopts["pass"]
+	 //                )
+	 // );
 
-	<?php
 	    $myPDO = pg_connect('host=ec2-54-235-90-107.compute-1.amazonaws.com dbname=dkf3l4nu5m7vj user=qbxcasondnmcds password=0b12931d71c65eee9a2fa303bcd8c1b460ff1878e7d0cc9f0c10a0ad563a85bc');
 	    $result = $myPDO->pg_query("SELECT * FROM Users");
 	    $result = $result->execute();
