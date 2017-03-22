cc9e31d9e814870062bea71bfb5acef9c38bbc6e
Muzammil <muzzammilmuneer@gmail.com>
2017-03-22 16:22:08 +0500

    no message

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 	 //                    'pdo.password' => $dbopts["pass"]
 	 //                )
 	 // );
+	echo 'muzammil';
 
 	    $myPDO = pg_connect('host=ec2-54-235-90-107.compute-1.amazonaws.com dbname=dkf3l4nu5m7vj user=qbxcasondnmcds password=0b12931d71c65eee9a2fa303bcd8c1b460ff1878e7d0cc9f0c10a0ad563a85bc');
 	    $result = $myPDO->pg_query("SELECT * FROM Users");
